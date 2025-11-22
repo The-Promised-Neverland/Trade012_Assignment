97cf45ca5b9011ab837130732be72cccf3dd8631
Abhijit Roy <abhijitroysde@gmail.com>
2025-11-22 12:50:48 +0530

    Added changes

--- internal/models/request.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type CreateRewardRequest struct {
-	UserID int64   `json:"user_id" binding:"required"`
-	Stock  string  `json:"stock" binding:"required"`
-	Shares float64 `json:"shares" binding:"required"`
-}
